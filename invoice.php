47613339859a30a40d12882ae1941d9c26c36fd6
IkbarIbrahim <kopih501@gmail.com>
2023-12-06 23:29:49 +0700

    punya iqbil

---
+++ invoice.php
@@ -0,0 +1,135 @@
+<?php
+require_once __DIR__ . '/partials/navbar.php';
+require 'function.php';
+$id = $_GET['id'];
+$query1 = "SELECT * FROM orders INNER JOIN users ON orders.user_id = users.id INNER JOIN fields ON field_id = fields.id WHERE id_transaksi = $id";
+$query = "SELECT * FROM transaksi INNER JOIN users ON orders.user_id = users.id INNER JOIN fields ON field_id = fields.id";
+$result = mysqli_query($conn, $query1);
+$row = mysqli_fetch_assoc($result);
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Invoice</title>
+    <link rel="stylesheet" type="text/css" href="iqbil/css/styles.css" />
+</head>
+
+<body>
+    <section>
+        <div class="invoice">
+            <div class="header">
+                <div class="i_row">
+                    <div class="i_logo">
+                        <p>Sportease</p>
+                    </div>
+                    <div class="i_title">
+                        <h2>INVOICE</h2>
+                        <!-- <p class="p_title text_right">April 20, 2023</p> -->
+                    </div>
+                </div>
+                <div class="i_row">
+                    <div class="i_number">
+                        <p class="p_title">INVOICE NO : 3452324</p>
+                    </div>
+                    <div class="i_address text_right">
+                        <!-- <p>TO</p> -->
+                        <p class="p_title">Date : 09-09-1909</p>
+                    </div>
+                </div>
+            </div>
+            <div class="body">
+                <div class="i_table">
+                    <div class="i_table_head">
+                        <div class="i_row">
+                            <div class="i_col w_14">
+                                <p class="p_title">QTY</p>
+                            </div>
+                            <div class="i_col w_55">
+                                <p class="p_title">DESCRIPTION</p>
+                            </div>
+                            <div class="i_col w_15">
+                                <p class="p_title">WAKTU SEWA</p>
+                            </div>
+                            <div class="i_col w_15">
+                                <p class="p_title">PRICE</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="i_table_body">
+
+                        <?php
+                        $total_qty = 0; // Variabel untuk menyimpan jumlah ID transaksi
+                        while ($row = mysqli_fetch_array($result)) {
+                            $total_qty++; ?>
+                            <div class="i_row">
+                                <div class="i_col w_14">
+                                    <p><?= $total_qty; ?></p>
+                                </div>
+                                <div class="i_col w_55">
+                                    <p><?= $row['name']; ?></p>
+                                </div>
+                                <div class="i_col w_15">
+                                    <p><?= $row['waktu_sewa']; ?></p>
+                                </div>
+                                <div class="i_col w_15">
+                                    <p>$<?= $row['price']; ?></p>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                    <div class="i_table_foot">
+                        <div class="i_row">
+                            <div class="i_col w_15">
+                                <p></p>
+                            </div>
+                            <div class="i_col w_55">
+                                <p></p>
+                            </div>
+                            <div class="i_col w_15">
+                                <p>Sub Total</p>
+                                <p>Tax 10%</p>
+                            </div>
+                            <div class="i_col w_15">
+                                <p>$150.00</p>
+                                <p>$15.00</p>
+                            </div>
+                        </div>
+                        <div class="i_row grand_total_wrap">
+                            <div class="i_col w_50"></div>
+                            <div class="i_col w_50 grand_total">
+                                <p>
+                                    <span>GRAND TOTAL:</span>
+                                    <span>$165.00</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="i_row">
+                    <div class="i_col w_50">
+                        <p class="p_title">Payment Method</p>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                            Cumque, dicta distinctio! Laudantium voluptatibus est nemo.
+                        </p>
+                    </div>
+                    <div class="i_col w_50 text_right">
+                        <p class="p_title">Terms and Conditions</p>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                            Cumque, dicta distinctio! Laudantium voluptatibus est nemo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
+
+<?php require_once __DIR__ . '/partials/footer.php'; ?>
